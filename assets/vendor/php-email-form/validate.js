064569dd14fd0c35f140ae6096a24547901edcb6
JayAlwaysCodes <johnsonjardocs@gmail.com>
2022-07-03 03:14:46 +0100

    added testimonials

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -1,8 +1,4 @@
-/**
-* PHP Email Form Validation - v3.2
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
+
 (function () {
   // "use strict";
 
@@ -49,32 +45,32 @@
     });
   });
 
-  function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
-      method: 'POST',
-      body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
-    })
-    .then(response => {
-      if( response.ok ) {
-        return response.text()
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
-  }
+  // function php_email_form_submit(thisForm, action, formData) {
+  //   fetch(action, {
+  //     method: 'POST',
+  //     body: formData,
+  //     headers: {'X-Requested-With': 'XMLHttpRequest'}
+  //   })
+  //   .then(response => {
+  //     if( response.ok ) {
+  //       return response.text()
+  //     } else {
+  //       throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+  //     }
+  //   })
+  //   .then(data => {
+  //     thisForm.querySelector('.loading').classList.remove('d-block');
+  //     if (data.trim() == 'OK') {
+  //       thisForm.querySelector('.sent-message').classList.add('d-block');
+  //       thisForm.reset(); 
+  //     } else {
+  //       throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     displayError(thisForm, error);
+  //   });
+  // }
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
