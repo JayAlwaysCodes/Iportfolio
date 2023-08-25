873f22e0117ec8b5dc678a5d6eaf7745269980c8
JayAlwaysCodes <johnsonjardocs@gmail.com>
2023-08-25 18:10:26 +0100

    made few edits

--- index.php
+++ index.php
@@ -372,9 +372,9 @@
 
           <div class="col-lg-4 col-md-6 portfolio-item filter-web">
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
+              <video src="assets/img/portfolio/Ecommerce-Trim.mp4" class="img-fluid" autoplay loop preload="auto"></video>
               <div class="portfolio-links">
-                <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
+                <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
                 <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
